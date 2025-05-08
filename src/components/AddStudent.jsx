4284d3f40a49ac85be6db0b00382fba04db21e25
shokhruzbek <ashokhruzbek@gmail.com>
2025-05-08 13:16:42 +0500

    some updates

--- src/components/AddStudent.jsx
+++ src/components/AddStudent.jsx
@@ -4,34 +4,34 @@ import { addStudent, removeStudent } from '../redux/studentSlice'
 import "../App.css"
 
 const AddStudent = ({ students }) => {
-    const [name, setName] = useState("")
-    const dispatch = useDispatch()
+  const [name, setName] = useState("")
+  const dispatch = useDispatch()
 
-    const handleAdd = () => {
-        students.includes(name) ? alert("Student already exists") : dispatch(addStudent(name))
-        setName("")
-    }
+  const handleAdd = () => {
+    students.includes(name) ? alert("Student already exists") : dispatch(addStudent(name))
+    setName("")
+  }
 
-    const handleRemove = (index) => dispatch(removeStudent(index))
+  const handleRemove = (index) => dispatch(removeStudent(index))
 
-    return (
-        <div>
-            <div>
-                <input
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    placeholder="Enter student name"
-                />
-                <button onClick={handleAdd}>Add</button>
-            </div>
-            <div>
-                {students.map((student, index) => (
-                    <p key={index}>{student} <span className='cursor' onClick={() => handleRemove(index)}>❌</span></p>
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter student name"
+        />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <div>
+        {students.map((student, index) => (
+          <p key={index}>{student} <span className='cursor' onClick={() => handleRemove(index)}>❌</span></p>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default AddStudent
